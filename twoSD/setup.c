c14790f45771df94939df437d26f1ff5b7301f23
Harsha Gangammanavar <gjharsha@gmail.com>
2018-01-10 15:37:07 -0600

    2SD update: Replication error fixed by cleaning the omegaType structure.

--- twoSD/setup.c
+++ twoSD/setup.c
@@ -228,6 +228,7 @@ int cleanCellType(cellType *cell, probType *prob, vector xk) {
 	if (cell->delta) freeDeltaType(cell->delta, cell->lambda->cnt, cell->omega->cnt, TRUE);
 	if (cell->lambda) freeLambdaType(cell->lambda, TRUE);
 	if (cell->sigma) freeSigmaType(cell->sigma, TRUE);
+	if (cell->omega) freeOmegaType(cell->omega, TRUE);
 
 	/* reset all the clocks */
 	cell->time.repTime = cell->time.iterTime = cell->time.masterIter = cell->time.subprobIter = cell->time.optTestIter = cell->time.argmaxIter = 0.0;
